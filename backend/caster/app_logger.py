f29e1fcbba378ff16c75f87ae3520d97252924bb
Iliya <iliyagonchar12345@gmail.com>
2022-11-08 01:34:17 +0300

    +abs_path

--- backend/caster/app_logger.py
+++ backend/caster/app_logger.py
@@ -1,10 +1,16 @@
 import logging
-
+from pathlib import Path
 
 _log_format = f'%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s'
 
+def get_absolute_dir_path() -> str:
+    path = Path(__file__).parent.resolve()
+
+    return path
+
+
 def get_file_handler() -> logging.FileHandler:
-    file_handler = logging.FileHandler('caster.log')
+    file_handler = logging.FileHandler()
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(logging.Formatter(_log_format))
     return file_handler
