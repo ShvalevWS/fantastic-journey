6c03ddfa26f9bb61265e2f3af863e4e91fbb9260
Iliya <iliyagonchar12345@gmail.com>
2023-01-13 18:07:05 +0300

    changes

--- arm64-autoinstall.sh
+++ arm64-autoinstall.sh
@@ -4,12 +4,6 @@ sudo apt-get update
 
 sudo apt-get install ca-certificates curl gnupg lsb-release
 
-git clone https://github.com/SergeiR13/fantastic-journey
-
-cd fantastic-journey
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-wget https://raw.githubusercontent.com/ShvalevWS/fantastic-journey/main/docker-compose.yml
 
 docker compose -f docker-compose.yml up -d
